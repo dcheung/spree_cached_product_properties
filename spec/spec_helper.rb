22cd29738d3635a3492b75b90e39d914342b2a0f
Chris Bell <cjbell1988@gmail.com>
2014-06-26 11:41:03 +0100

    Fix missing factory file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,9 +29,6 @@ require 'spree/testing_support/controller_requests'
 require 'spree/testing_support/authorization_helpers'
 require 'spree/testing_support/url_helpers'
 
-# Requires factories defined in lib/spree_cached_product_properties/factories.rb
-require 'spree_cached_product_properties/factories'
-
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
 
